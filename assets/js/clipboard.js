887c2c25170c2538d9ac8eaeb41ea2fb2d05e096
RossageRoll <65648962+RossageRoll@users.noreply.github.com>
2022-09-18 00:28:53 +1000

    Further Updates

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,15 +1,81 @@
-//Joint Subscriptions
-function SubscriptionsFunction() {
-    var copyText = document.getElementById("SubscriptionsInput");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
+// Accounts
 
-    var tooltip = document.getElementById("SubscriptionsTooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
-  }
+    //Personal Expenses 🌊
+    function PersExpFunction() {
+        var copyText = document.getElementById("PersExpInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
 
-  function SubscriptionsoutFunc() {
-    var tooltip = document.getElementById("SubscriptionsTooltip");
-    tooltip.innerHTML = "Copy to Clipboard";
-  }
+    // Personal Savings 💰
+    function PersSaveFunction() {
+        var copyText = document.getElementById("PersSaveInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    // Perosnal Lifesaver 🌌
+    function PersLifeFunction() {
+        var copyText = document.getElementById("PersLifeInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    // Joint Bills 🏡
+    function JointBillsFunction() {
+        var copyText = document.getElementById("JointBillsInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        }    
+
+    // Joint Spending 🛒
+    function JointSpendFunction() {
+        var copyText = document.getElementById("JointSpendInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    //Joint Subscriptions 💾
+    function JointSubFunction() {
+        var copyText = document.getElementById("JointSubInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    //Joint Savings 👪
+    function JointSaveFunction() {
+        var copyText = document.getElementById("JointSaveInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    //Joint Emergency Fund 🚨
+    function JointEmergFunction() {
+        var copyText = document.getElementById("JointEmergInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    //Cooper Spending 👶
+    function CoopSpendFunction() {
+        var copyText = document.getElementById("CoopSpendInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
+
+    // Cooper Lifesaver 💸
+    function CoopLifeFunction() {
+        var copyText = document.getElementById("CoopLifeInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    }
